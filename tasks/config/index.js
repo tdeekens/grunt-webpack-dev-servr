50e9f82ebd0220930344b6d5c40509c1e940584a
Gunnar Wrobel <p@rdus.de>
2015-06-18 13:32:49 +0200

    Add host and port parameter for the server

--- tasks/config/index.js
+++ tasks/config/index.js
@@ -1,15 +1,24 @@
 'use strict';
 
 function Config(config) {
+  this._config = {
+    server: {}
+  };
   if (config.config) {
-    this._config = require(config.config);
+    this._config.webpack = require(config.config);
   } else {
-    this._config = config;
+    this._config.webpack = config;
   }
+  this._config.server.host = config.host || 'localhost';
+  this._config.server.port = config.port || 8080;
 }
 
 Config.prototype.webpack = function() {
-  return this._config;
+  return this._config.webpack;
+};
+
+Config.prototype.server = function() {
+  return this._config.server;
 };
 
 module.exports = Config;
--- test/config/index.spec.js
+++ test/config/index.spec.js
@@ -4,19 +4,49 @@ var expect = require('chai').expect,
     Config = require('../../tasks/config'),
     config;
 
-describe('Config', function() {
+describe('Configuration', function() {
 
-  it('should offer a webpack config', function() {
-    config = new Config({});
+  describe('the webpack part', function() {
 
-    expect(config.webpack()).is.a('object');
-  });
+    it('should offer a webpack config', function() {
+      config = new Config({});
 
-  it('should load a webpack config', function() {
-    config = new Config({
-      config: '../fixtures/basic.js'
+      expect(config.webpack()).is.a('object');
     });
 
-    expect(config.webpack()).to.deep.equal({a: 1});
+    it('should load a webpack config', function() {
+      config = new Config({
+        config: '../fixtures/basic.js'
+      });
+
+      expect(config.webpack()).to.deep.equal({a: 1});
+    });
+
+  });
+
+  describe('the server part', function() {
+
+    it('should offer a server config', function() {
+      config = new Config({});
+
+      expect(config.server()).is.a('object');
+    });
+
+    it('should provide a host parameter', function() {
+      config = new Config({
+        host: 'x'
+      });
+
+      expect(config.server().host).to.equal('x');
+    });
+
+    it('should provide a port parameter', function() {
+      config = new Config({
+        port: 80
+      });
+
+      expect(config.server().port).to.equal(80);
+    });
+
   });
 });
