b6bf0896d7b8ac926353d3e8fe2c4aa8cfb040b7
Gunnar Wrobel <p@rdus.de>
2015-06-22 19:16:21 +0200

    Allow to start a server using the config

--- tasks/server/index.js
+++ tasks/server/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
-function Server(options) {
-  this._options = options;
+function Server(_Server, webpack) {
+  this._Server = _Server;
+  this._webpack = webpack;
 }
 
-Server.prototype.noop = function() {
-  return true;
+Server.prototype.start = function(options) {
+  var _serverOptions = options.server(),
+      server = new this._Server(
+        this._webpack(options.webpack()),
+        _serverOptions.options
+      );
+  server.listen(_serverOptions.port, _serverOptions.host, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+
+  return server;
 };
 
 module.exports = Server;
--- test/server/index.spec.js
+++ test/server/index.spec.js
@@ -1,16 +1,57 @@
-var
-  expect = require('chai').expect;
+'use strict';
 
-var
-  Server = require('../../tasks/server'),
-  server;
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    Server = require('../../tasks/server'),
+    listen,
+    options,
+    webpack,
+    webpackServer,
+    server;
 
-describe('Server specification', function() {
+describe('Server', function() {
+
   beforeEach(function() {
-    server = new Server();
+    options = {
+      webpack: sinon.stub(),
+      server: sinon.stub()
+    };
+    listen = sinon.spy();
+    webpack = sinon.stub();
+    webpackServer = sinon.stub();
+    webpackServer.returns({listen: listen});
   });
 
-  it('should ensure that something truethy is meant to be true', function() {
-    expect(+[]).to.equal(0);
+  it('should create a webpack object using webpack options', function() {
+
+    options.server.returns({});
+    options.webpack.returns({a: 1});
+
+    server = new Server(webpackServer, webpack);
+    server.start(options);
+    expect(webpack.callCount).to.equal(1);
+    expect(webpack.firstCall.calledWith({a: 1})).to.be.ok;
+  });
+
+  it('should create a server object using webpack and options', function() {
+    options.server.returns({options: 'OPTIONS'});
+
+    webpack.returns('WEBPACK');
+
+    server = new Server(webpackServer, webpack);
+    server.start(options);
+
+    expect(webpackServer.callCount).to.equal(1);
+    expect(webpackServer.firstCall.calledWith('WEBPACK', 'OPTIONS')).to.be.ok;
+  });
+
+  it('should listen to the given host and port', function() {
+    options.server.returns({host: 'HOST', port: 'PORT'});
+
+    server = new Server(webpackServer, webpack);
+    server.start(options);
+
+    expect(listen.callCount).to.equal(1);
+    expect(listen.firstCall.calledWith('PORT', 'HOST')).to.be.ok;
   });
 });
